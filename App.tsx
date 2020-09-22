99e515cbd910ce54a1dddfcfbaebef8f02d749d1
unknown <01secn@gmail.com>
2020-09-22 15:28:50 +0100

    First commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform } from 'react-native';
+import { Button } from 'react-native-elements';
+import styled from 'styled-components/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App: React.FC = () => (
+  <Page>
+    <Header>Welcome to Impact! Select to start / stop recording your journeys</Header>
+    <ButtonsContainer>
+      <Button title='Start' buttonStyle={{ height: 76 }} titleStyle={{ fontSize: 24 }} />
+      <Button buttonStyle={{ marginTop: 28, height: 76 }} titleStyle={{ fontSize: 24 }} title='Stop' />
+    </ButtonsContainer>
+  </Page>
+);
+
+const Header = styled.Text`
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 18px;
+`;
+
+const ButtonsContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Page = styled.SafeAreaView`
+  height: 100%;
+  width: 100%;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-top: ${isAndroid() ? '54px' : '0'}
+  padding-bottom: ${isAndroid() ? '54px' : '0'}
+  display: flex;
+  justify-content: center;
+`;
+
+function isAndroid(): boolean {
+  return Platform.OS === 'android';
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
